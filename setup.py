901d19ba98358e7c7e94144989e6b759064cbfb7
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2024-10-13 11:38:56 +0200

    added long description from readme

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name='bin2tap',
-    version='0.1',
+    version='0.3',
     author='Raül Torralba',
     description='Convert a binary file into ZX Spectrum TAP file',
+    long_description=README,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
     entry_points='''
