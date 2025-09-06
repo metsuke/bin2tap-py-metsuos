7fbf469bd86585c5b6bafeb6d09072bcf759fe1f
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2025-09-06 15:50:07 +0200

    chore: rename project folder for disambiguation with skoolkit

--- setup.py
+++ setup.py
@@ -6,16 +6,18 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='bin2tap',
-    version='0.3',
+    version='0.5',
     author='Raül Torralba',
     description='Convert a binary file into ZX Spectrum TAP file',
     long_description=README,
     long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
-    entry_points='''
-        [console_scripts]
-        bin2tap=bin2tap.cli:main
-    ''',
+    entry_points={
+        'console_scripts': [
+            'bin2tap=zxbin2tap.cli:main',
+            'zxbin2tap=zxbin2tap.cli:main',
+        ],
+    },
     url = 'https://github.com/rtorralba/bin2tap',
 )
